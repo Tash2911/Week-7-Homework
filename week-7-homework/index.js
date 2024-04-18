8bad6c0c42fcd449bd59797dc7cdf2770a614ef9
Tash2911 <160403113+Tash2911@users.noreply.github.com>
2024-04-18 21:09:22 +0200

    created java script file

---
+++ week-7-homework/index.js
@@ -0,0 +1,6 @@
+function submit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#submit");
+}
+let searchInputElement = document.querySelector("#search-Input");
+searchInputElement.addEventListener("search", submit);
