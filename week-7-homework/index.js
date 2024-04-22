c0f44d90793b671f88cba477d1e3a5a459550a92
Tash2911 <160403113+Tash2911@users.noreply.github.com>
2024-04-22 14:29:25 +0200

    added search api and functionality

--- week-7-homework/index.js
+++ week-7-homework/index.js
@@ -1,8 +1,22 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+}
+function searchCity(city) {
+  let apiKey = "5804cet5d8681bbaa2c0f31803d6o3f5";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}& units=metric or imperial`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 function SubmitEvent(event) {
   event.preventDefault();
   searchInput = document.querySelector("#placeholder");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchform = document.querySelector("#search-form");
 searchform.addEventListener("submit", SubmitEvent);
+searchCity("Harare");
