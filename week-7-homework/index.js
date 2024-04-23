169525c2aebe929a648eee3e95318d0e39abb726
Tash2911 <160403113+Tash2911@users.noreply.github.com>
2024-04-23 20:13:06 +0200

    added icon url and functionality

--- week-7-homework/index.js
+++ week-7-homework/index.js
@@ -7,7 +7,9 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="temperature-icon"/>`;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
